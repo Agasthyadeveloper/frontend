e5dec0c97db04b46066ed17851a8bf122d25002c
gopinathayyavu <gopinath0028@gmail.com>
2024-12-10 11:27:20 +0530

    first commit

---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-A7LSO3LK.mjs";import"./chunk-OMQEU7GS.mjs";import"./chunk-45ZE5YMO.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-YJRZK3GP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
