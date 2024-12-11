7215140b702be5a997a6adf3749ace5d13948fcc
gopinathayyavu <gopinath0028@gmail.com>
2024-12-11 12:39:29 +0530

    update

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-VQ53S2DT.mjs";import"./chunk-OMQEU7GS.mjs";import"./chunk-45ZE5YMO.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-YJRZK3GP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-NLRVOSXC.mjs";import"./chunk-OMQEU7GS.mjs";import"./chunk-45ZE5YMO.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-YJRZK3GP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
