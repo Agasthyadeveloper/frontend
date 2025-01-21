8f5e11df06dbbb50f613a79f6a50d75b71dbe932
gopinathayyavu <gopinath0028@gmail.com>
2025-01-21 13:39:10 +0530

    depratment-change

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-4W2LACSU.mjs";import"./chunk-2VWKLFA5.mjs";import"./chunk-SGDFFW6V.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-YJRZK3GP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-BK6W5ULR.mjs";import"./chunk-2VWKLFA5.mjs";import"./chunk-SGDFFW6V.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-YJRZK3GP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
