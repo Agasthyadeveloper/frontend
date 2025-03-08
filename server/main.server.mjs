3c0501ca2ea4137a186f9fad89722618b92f7b04
gopinathayyavu <gopinath0028@gmail.com>
2025-03-08 17:47:40 +0530

    font-changed

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-6XYA6P7V.mjs";import"./chunk-JD332WWQ.mjs";import"./chunk-6R6APHXY.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-YJRZK3GP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-BETVKD4L.mjs";import"./chunk-JD332WWQ.mjs";import"./chunk-6R6APHXY.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-YJRZK3GP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
