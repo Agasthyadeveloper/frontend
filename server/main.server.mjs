db4df933c5baed502df142e01d8e561f224aeef8
gopinathayyavu <gopinath0028@gmail.com>
2025-06-24 17:57:59 +0530

    prod 24/06/25 5

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-OQR5ARYM.mjs";import"./chunk-6ZUCF3XD.mjs";import"./chunk-WQ73NK65.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-YJRZK3GP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-ODMZB5M7.mjs";import"./chunk-6ZUCF3XD.mjs";import"./chunk-WQ73NK65.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-YJRZK3GP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
