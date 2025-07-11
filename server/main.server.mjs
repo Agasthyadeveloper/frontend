f2e5599dd1ec34093cff8876bb668b9e46927984
gopinathayyavu <gopinath0028@gmail.com>
2025-07-11 11:12:27 +0530

    pord-11-07-25

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-VPWD7J7W.mjs";import"./chunk-6ZUCF3XD.mjs";import"./chunk-WQ73NK65.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-YJRZK3GP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-D2AND3MR.mjs";import"./chunk-6ZUCF3XD.mjs";import"./chunk-WQ73NK65.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-YJRZK3GP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
