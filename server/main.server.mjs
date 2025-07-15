78a652e4dd448fe369eb8649643264e7fd1b3df3
gopinathayyavu <gopinath0028@gmail.com>
2025-07-15 12:04:40 +0530

    put

---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-ZORUKJGU.mjs";import"./chunk-6ZUCF3XD.mjs";import"./chunk-WQ73NK65.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-YJRZK3GP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
