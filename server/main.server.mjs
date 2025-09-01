ac26c8c53a66725eb6ff04533700bc2a538db5f6
gopinathayyavu <gopinath0028@gmail.com>
2025-09-01 18:26:38 +0530

    liaison

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-VKNLYE5J.mjs";import"./chunk-4U5RWN3I.mjs";import"./chunk-UQLKTAIS.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-YJRZK3GP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-C6ZCYA3W.mjs";import"./chunk-4U5RWN3I.mjs";import"./chunk-UQLKTAIS.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-YJRZK3GP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
