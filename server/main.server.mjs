49b2003b066da2f428820423514d57ce1b1a203e
priyadevi-madeshvaren <priyadevi09404@gmail.com>
2026-01-12 17:34:47 +0530

    ui changes liaison

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-SRQ6EUNU.mjs";import"./chunk-Y7GI7AJ6.mjs";import"./chunk-JDWEMDTR.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-YJRZK3GP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-5PFSLRMD.mjs";import"./chunk-Y7GI7AJ6.mjs";import"./chunk-JDWEMDTR.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-YJRZK3GP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
