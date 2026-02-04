643332dc072e3495d7716ef43f8c78c9031c874f
priyadevi-madeshvaren <priyadevi09404@gmail.com>
2026-02-04 16:32:56 +0530

    implement hr module

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-5RUVNKJT.mjs";import"./chunk-E3NGYXIE.mjs";import"./chunk-PG4WULN5.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-YJRZK3GP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-ND7N5AUW.mjs";import"./chunk-E3NGYXIE.mjs";import"./chunk-PG4WULN5.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-YJRZK3GP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
