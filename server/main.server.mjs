ba9bc823fe24f040a6416af16ccc226ce803ecd7
priyadevi-madeshvaren <priyadevi09404@gmail.com>
2026-02-10 18:30:11 +0530

    implemented ui for dashbaord

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-N5SJJ5C7.mjs";import"./chunk-E3NGYXIE.mjs";import"./chunk-PG4WULN5.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-YJRZK3GP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-BQ2XKYXX.mjs";import"./chunk-E3NGYXIE.mjs";import"./chunk-PG4WULN5.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-YJRZK3GP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
