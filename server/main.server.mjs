1c78e3adc41342135de1abf460a662ddd4c9a7cc
priyadevi-madeshvaren <priyadevi09404@gmail.com>
2026-02-11 14:13:19 +0530

    updated dashboard ui

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-XHLMKMWY.mjs";import"./chunk-E3NGYXIE.mjs";import"./chunk-PG4WULN5.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-YJRZK3GP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-KSN7XRWG.mjs";import"./chunk-E3NGYXIE.mjs";import"./chunk-PG4WULN5.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-YJRZK3GP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
