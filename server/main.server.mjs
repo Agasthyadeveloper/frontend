ce499a6dae05fe3d0c26bd538ca667353f1a62e5
priyadevi-madeshvaren <priyadevi09404@gmail.com>
2026-02-24 16:23:22 +0530

    Implemented marketing,civil,sales module

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-S5LGBBQY.mjs";import"./chunk-E3NGYXIE.mjs";import"./chunk-PG4WULN5.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-YJRZK3GP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-ZQ4YWGPJ.mjs";import"./chunk-E3NGYXIE.mjs";import"./chunk-PG4WULN5.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-YJRZK3GP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
