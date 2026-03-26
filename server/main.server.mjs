6fc549345166d14980504f38eb247631f789081f
priyadevi-madeshvaren <priyadevi09404@gmail.com>
2026-03-26 16:13:49 +0530

    Implemented sales and hr module

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-BMLNRWK4.mjs";import"./chunk-LDXWL3MV.mjs";import"./chunk-NIQEM2V5.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-YJRZK3GP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-4ZBRG6LP.mjs";import"./chunk-LDXWL3MV.mjs";import"./chunk-NIQEM2V5.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-YJRZK3GP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
