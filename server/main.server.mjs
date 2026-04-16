8dab5e6a08e48452a87c38ade423020349733ee2
priyadevi-madeshvaren <priyadevi09404@gmail.com>
2026-04-16 15:26:35 +0530

    Implemented meeting module for all departments

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-SDLI7K7R.mjs";import"./chunk-3IJRYAWP.mjs";import"./chunk-OYGNC3AA.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-YJRZK3GP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-FXWRVL76.mjs";import"./chunk-3IJRYAWP.mjs";import"./chunk-OYGNC3AA.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-YJRZK3GP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
