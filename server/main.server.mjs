48e4f3dd90963375feeaed5aba2a189ba7fffd39
priyadevi-madeshvaren <priyadevi09404@gmail.com>
2026-04-22 17:55:29 +0530

    fixed product bugs in marketing department

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-H3JKUT5W.mjs";import"./chunk-KKW6TGRJ.mjs";import"./chunk-VG3D6BDQ.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-YJRZK3GP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-HCBMSFMN.mjs";import"./chunk-KKW6TGRJ.mjs";import"./chunk-VG3D6BDQ.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-YJRZK3GP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
