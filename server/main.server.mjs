b0de9ece3d0a6f1000f896fb59302dd1c5046958
Sureshkumar <sureshkumar9887@gmail.com>
2026-09-01 10:31:27 +0530

    01092026_1031 - Changes are published to Production

---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-KLF2H7MI.mjs";import"./chunk-3KZ5UOXX.mjs";import"./chunk-3FPKB57C.mjs";import"./chunk-DCFYTP2A.mjs";export{a as default};
---
+++ server/render-utils.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{n as o,o as r,p as n}from"./chunk-3KZ5UOXX.mjs";import{rc as e}from"./chunk-3FPKB57C.mjs";import"./chunk-DCFYTP2A.mjs";export{n as renderApplication,r as renderModule,e as \u0275Console,o as \u0275SERVER_CONTEXT};
